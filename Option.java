2ae16526a0a9c08a5759c4911c18848d01764148
Victor <victormel87@gmail.com>
2016-04-25 10:50:46 +0200

    Creada clase enum Option

---
+++ Option.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Option here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public enum Option{
+    GO, QUIT, HELP, LOOK, EAT, BACK, TAKE, DROP, ITEMS
+}
+
