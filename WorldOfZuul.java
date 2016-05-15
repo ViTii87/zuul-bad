2b2b809e25db689e7f5f072230ae51a990efda84
Victor <victormel87@gmail.com>
2016-05-15 20:45:17 +0200

    Añadida clase WorldOfZuul y añadido el metodo main

---
+++ WorldOfZuul.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class WorldOfZuul here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WorldOfZuul
+{
+    private static Game juego;
+
+    public static void main(String[] args) {
+        juego = new Game();
+        juego.play();
+    }
+}
